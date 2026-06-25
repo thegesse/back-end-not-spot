13cd2905fbd57bea4e1b114d2c7abf29a1f1e56a
Esteban Quillerier <emquillerier@gmail.com>
2026-06-25 21:36:26 +0200

    Implement CORS configuration in SecurityConfig
    Added CORS configuration to allow specific origins and methods.

--- src/main/java/com/goose/notspot/configuration/SecurityConfig.java
+++ src/main/java/com/goose/notspot/configuration/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
@@ -23,7 +24,26 @@ public class SecurityConfig {
                 )
                 .build();
     }
-
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        // whitelist des domaines autoriser a se connecter a l'api
+        configuration.setAllowedOrigins(List.of(
+            "https://thegesse.github.io", // prod temporaire, juste le temps de l'eval
+            "https://app.kaillou.de",     // TODO creer un sous domaine app.kaillou.de
+        ));
+        
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); 
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
