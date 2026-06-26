604e89a2251c1c369ab043acc1c5da6960437e60
thegesse <goose.minipainting@gmail.com>
2026-06-26 20:58:38 +0200

    added missing imports

--- src/main/java/com/goose/notspot/configuration/SecurityConfig.java
+++ src/main/java/com/goose/notspot/configuration/SecurityConfig.java
@@ -2,10 +2,16 @@ package com.goose.notspot.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -34,7 +40,7 @@ public class SecurityConfig {
             "https://thegesse.github.io", // prod temporaire, juste le temps de l'eval
             "https://app.kaillou.de",     // TODO creer un sous domaine app.kaillou.de
             "http://localhost:3000",
-            "http://localhost:8080", //local aussi
+            "http://localhost:8080"//local aussi
         ));
         
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
